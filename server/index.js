8f707bf15339a369d23ca5da97744485d28677bc
MuhammadBilal561 <bilalrehan2006@gmail.com>
2025-08-31 08:40:57 +0500

    update

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use("/", dalleRoutes);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Backend is running 🚀" });
+  res.status(200).json({ message: "Backend is running " });
 });
 
 // ❌ DO NOT listen(); Vercel handles it
